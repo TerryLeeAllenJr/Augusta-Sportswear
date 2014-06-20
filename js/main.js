a727486de23fb7d9b5c7276471b81545aedd8c0d
Lee Allen <lee.allen.sc@gmail.com>
2014-06-20 04:27:03 -0400

    Completed core functionality. Modified media queries to display on mobile

--- js/main.js
+++ js/main.js
@@ -1,23 +1,25 @@
+/* Please note that this is not my customary way of coding. Generally, I would have refactored multiple times before turning
+    over code for consideration. There are still some bugs, and there are MANY things that I would do differently in the long
+    run. I will continue to clean this code up in the coming days.
+ */
+
+
 $(document).ready(function(){
 
-    // Set up our order obkect.
 
-    var currentStep = 0;
-
+    // Get rid of global vars.
+    var currentStep = 0,
+        steps = ['Select a Product','Select a Cut','Select a Design','Select a Style'],
+        stepPlugs = ['product','cuts','designs','decorations'],
+        productId = 0;
 
     var app = function(){
 
         this.currentOrder = new Object();
         this.products = new Object();
-        var steps = ['Select a Product','Select a Cut','Select a Design','Select a Style'];
 
 
-        // Necessarry?
-        this.drawProducts = function(products){
-
-        }
-
-
+        // This is the constructor for the app.
         this.bootstrap = function(){
 
             // Reset the order to null.
@@ -27,14 +29,14 @@ $(document).ready(function(){
             $('.app-selection-container').html("Loading...");
 
             // Get current product list. This will be modified to pull data from the real backend.
-            $.get('http://augusta.lee-allen.com/php/data.ajax.php',
+            $.post('http://augusta.lee-allen.com/php/data.ajax.php',
                 {
                     data: 'products'
                 },
                 function (response) {
                     $('.app-selection-container').html("");
                     $.each(response,function(key,value){
-                        var html = "<div class=\"col-lg-3 app-selection-panel\" data-type=\"product\">";
+                        var html = "<div class=\"col-lg-3 app-selection-panel\" data-type=\"product\" data-id=\""+key+"\">";
                         html = html+"<div class=\"app-selection-data\">";
                         html = html+"<img src=\"img/products/"+value.img+"\"/>";
                         html = html+"<h4>"+value.product+"</h4>";
@@ -51,7 +53,7 @@ $(document).ready(function(){
             currentOrder.product = null;
             currentOrder.cut = null;
             currentOrder.design = null;
-            currentOrder.style = null;
+            currentOrder.decoration = null;
         }
 
         // This is responsible for cycling through the currentOrder and redrawing the sidebar.
@@ -59,23 +61,51 @@ $(document).ready(function(){
             $('.sidebar-steps').html("");
             var html = "";
             $.each(currentOrder,function(key,value){
-
+                //alert(key+" "+value);
                 if(value){
                      html = html + "<li>"+value+"</li>";
                 }
             });
-            html = html+"<li><span class=\"sidebar-selection\">"+steps[currentStep]+"</span></li>";
+            if(typeof steps[currentStep]!== "undefined"){
+                html = html+"<li><span class=\"sidebar-selection\">"+steps[currentStep]+"</span></li>";
+            }
             $('.sidebar-steps').append(html);
         }
 
 
         // This sets the currentOrder obj when an image is clicked.
-        this.setCurrentOrder = function(type,value) {
+        this.setCurrentOrder = function(currentStep,value) {
             // Forgive this sloppy code. Much refactoring is in order.
-            if(type=='product'){currentOrder.product = value;}
-            if(type=='cut'){currentOrder.cut = value;}
-            if(type=='design'){currentOrder.cut = value;}
-            if(type=='decoration'){currentOrder.cut = value;}
+            if(stepPlugs[currentStep]=='product'){currentOrder.product = value;}
+            if(stepPlugs[currentStep]=='cuts'){currentOrder.cut = value;}
+            if(stepPlugs[currentStep]=='designs'){currentOrder.design = value;}
+            if(stepPlugs[currentStep]=='decorations'){currentOrder.decoration = value;}
+        }
+
+        // This redraws the selection area.
+        this.drawSelections = function(currentStep,productId){
+            var html = (currentStep+1)+".) "+steps[currentStep];
+
+            $('.step-header').html(html);
+
+            $.post('http://augusta.lee-allen.com/php/data.ajax.php',
+                {
+                    productId: productId,
+                    data: stepPlugs[currentStep]
+                },
+                function (response) {
+                    $('.app-selection-container').html("");
+                    $.each(response,function(key,value){
+                        var html = "<div class=\"col-lg-3 app-selection-panel\" data-type=\"product\" data-id=\""+productId+"\">";
+                        html = html+"<div class=\"app-selection-data\">";
+                        html = html+"<img src=\"img/products/"+value.img+"\"/>";
+                        html = html+"<h4>"+value.text+"</h4>";
+                        html = html+"</div>";
+                        html = html+"</div>";
+                        $('.app-selection-container').append(html);
+                    });
+                }, 'json');
+
         }
 
 
@@ -84,12 +114,22 @@ $(document).ready(function(){
 
 
 
+
+
     //Bootstrap the app.
     app.bootstrap();
-
     $('body').on('click', '.app-selection-panel', function() {
+        productId = $(this).data('id');
+        app.setCurrentOrder(currentStep,$(this).text());
         currentStep++;
-        app.setCurrentOrder($(this).data('type'),$(this).text());
         app.drawSidebar(app.currentOrder,currentStep);
+        if(currentStep < steps.length){
+            app.drawSelections(currentStep,$(this).data('id'));
+            $('.app-selection-panel').not(this).hide();
+            $(this).fadeOut(500);
+        }else{
+            $('.next-button').removeClass('disabled');
+            return false;
+        }
     });
 });
--- php/data.ajax.php
+++ php/data.ajax.php
@@ -2,28 +2,83 @@
 
 /*This file is only used to pass some "mock" data back to the primary app. */
 
-switch($_GET['data']){
-    case 'products':
 
-        echo json_encode(array(
-            0=>array(
-                'product'=>'Shooter Shirt',
+$products = array(
+    0=>array(
+        'product'=>'Shooter Shirt',
+        'img'=>'product-shooter-shirt.jpg',
+        'cuts'=>array(
+            0=> array(
+                'text'=>'Shooter Shirt',
                 'img'=>'product-shooter-shirt.jpg'
             ),
-            1=>array(
-                'product'=>'Jersey / Short',
-                'img'=>'product-jersey-short.jpg'
-            ),
-            2=>array(
-                'product'=>'Test Product 3',
+            1=> array(
+                'text'=>'Test Cut 1',
                 'img'=>'no-image.jpg'
             ),
-            3=>array(
-                'product'=>'Test Product 4',
+            2=> array(
+                'text'=>'Test Cut 2',
                 'img'=>'no-image.jpg'
+            )
+        ),
+        'designs'=>array(
+            0=> array(
+                'text'=>'Demolition',
+                'img'=>'design-demolition.jpg'
+            ),
+            1=> array(
+                'text'=>'Throttle',
+                'img'=>'design-throttle.jpg'
+            ),
+            2=> array(
+                'text'=>'Test Design 1',
+                'img'=>'no-image.jpg'
+            ),
+            3=> array(
+                'text'=>'Test Design 2',
+                'img'=>'no-image.jpg'
+            )
+        ),
+        'decorations'=>array(
+            0=> array(
+                'text'=>'Front Mascot - Back 2" & 8"',
+                'img'=>'decoration-front-mascot.jpg'
+            ),
+            1=> array(
+                'text'=>'Front Arched 3" - Back 2" & 8"',
+                'img'=>'decoration-front-arched.jpg'
+            ),
+            2=> array(
+                'text'=>'Front 3" - Back 2" & 8"',
+                'img'=>'decoration-front-3.jpg'
+            ),
+            3=> array(
+                'text'=>'Test Decoration 1',
+                'img'=>'no-image.jpg'
+            )
+        )
+    ),
+    1=>array(
+        'product'=>'Jersey / Short',
+        'img'=>'product-jersey-short.jpg'
+    ),
+    2=>array(
+        'product'=>'Test Product 3',
+        'img'=>'no-image.jpg'
+    ),
+    3=>array(
+        'product'=>'Test Product 4',
+        'img'=>'no-image.jpg'
     )
-        ));
-        break;
+);
+
+
+
+if($_POST['data']=='products'){
+    echo json_encode($products);
+    die;
+}else{
+    echo json_encode($products[$_POST['productId']][$_POST['data']]);
 }
 
 
